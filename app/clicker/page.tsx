18dc7a36700967f0ea3d2c1164b3f48d67686907
CD-338080 <LouisWB99933@proton.me>
2025-10-21 02:02:36 -0600

    hola

--- app/clicker/page.tsx
+++ app/clicker/page.tsx
@@ -60,7 +60,7 @@ function ClickerPage() {
             case 'settings':
                 return <Settings setCurrentView={setCurrentView} />;
             case 'mine':
-                return <Mine setCurrentView={setCurrentView} />;
+                return <Mine currentView={currentView} setCurrentView={setCurrentView} />;
             case 'friends':
                 return <Friends />;
             case 'earn':
